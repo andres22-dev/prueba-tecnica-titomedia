1638d64112c0192a73ee319b543129ea544afa25
andres22 <afpinchao8@misena.edu.co>
2021-02-24 01:10:31 -0500

    funciones asincronas para realizar peticiones y recibir imagenes completada solo falta agregarla a los demas nodos

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,33 @@
+const url = `https://rickandmortyapi.com/api/character/`;
+
+
+
+
+const petitionCharacter = async (url) =>{
+
+  const petition = await fetch(url);
+  const response = await petition.json();
+  const character = await response;
+
+  return character;
+
+}
+
+const addCharacter = async() =>{
+
+  const rick = await petitionCharacter(`${url}1`);
+  const img = document.querySelector('#img1')
+  const createImage = document.createElement('img');
+  createImage.src = rick.image
+  img.append(createImage);
+  
+  
+
+  
+}
+
+addCharacter();
+
 addEventListener('DOMContentLoaded', () => {
 
   const botonMenu = document.querySelector('#buttonR');
@@ -14,5 +44,3 @@ addEventListener('DOMContentLoaded', () => {
 
 
 })
-
-
